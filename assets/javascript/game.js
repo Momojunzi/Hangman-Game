e80227f8829634ac220b64e43b85cc1149fc3252
Momojunzi <mark.holthaus@gmail.com>
2017-07-31 20:40:44 -0700

    basic game without winning or losing

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -4,6 +4,10 @@ var game = {
 	nameArr: [],
 	guesses: 0,
 	guessedLetters: [],
+	formattedName: [[], []],
+	firstSpaces: [],
+	lastSpaces: [],
+	chosenKey: "",
 	gameNames: [
 				"Jon Snow", "Robb Stark", "Eddard Stark", "Arya Stark", "Brandon Stark", 
 				"Daenerys Targaryen", "Petyr Baelish","Tyrion Lannister", "Theon Greyjoy", 
@@ -19,38 +23,63 @@ var game = {
 				gameplay.style.display = "block";
 				game.gameStarted = true;
 				game.chooseName();
-				game.gamefunction();
-				console.log(game.nameArr.toString(), game.guesses, game.guessedLetters);
+				game.keyChooser();
+				console.log(game.gameStarted);
 			}
 		}
 	},
 	chooseName: function() {
 		if(this.gameStarted === true) {
-			var nameIndex = Math.floor(Math.random() * (this.gameNames.length - 1));
+			var nameIndex = Math.floor(Math.random() * this.gameNames.length);
 			var name = this.gameNames[nameIndex].toLowerCase();
-			for(var index=0; index < name.length; index++) {
-				this.nameArr.push(name[index]);
-			}
-			this.guesses = this.nameArr.length + 2;
+			var tempName = name.split(" ");
+			this.formattedName[0] = tempName[0].split('');
+			this.formattedName[1] = tempName[1].split('');
+			for (var i=0; i<this.formattedName[0].length; i++){
+                this.firstSpaces.push("_");
+            }
+            for (var j=0; j<this.formattedName[1].length; j++){
+                this.lastSpaces.push("_");
+            }
+            document.getElementById('first').innerHTML = this.firstSpaces.join(' ');
+            document.getElementById('last').innerHTML = this.lastSpaces.join(' ');
+            console.log(this.formattedName[0], this.formattedName[1], this.firstSpaces, this.lastSpaces);
 		}
 	},
-	gamefunction: function () {
+	keyChooser: function() {
 		if(this.gameStarted === true) {
 			document.onkeyup = function(event) {
-				var key = event.key;
-				if(game.nameArr.indexOf(key) === -1) {
-					game.guesses--;
-					game.guessedLetters.push(key);
-					console.log(game.guesses.length, game.guesses, game.guessedLetters);
-				}	
+				game.chosenKey = event.key;
+				game.letterCompare();
 			}
-			
+		}
+		
+	},
+	letterCompare: function() {
+		if(this.formattedName[0].indexOf(this.chosenKey) === -1 && this.formattedName[1].indexOf(this.chosenKey) === -1) {
+			this.guessedLetters.push(this.chosenKey);
+			document.getElementById('used').innerHTML = this.guessedLetters.join(' ');
+		}
+		if(this.formattedName[0].indexOf(this.chosenKey) > -1  || this.formattedName[1].indexOf(this.chosenKey) > -1) {
+			for(var i=0; i<this.formattedName[0].length; i++) {
+				 if(this.chosenKey === this.formattedName[0][i]){
+				 	this.firstSpaces[i] = this.chosenKey;
+				 }
+			}
+			for(var j=0; j<this.formattedName[1].length; j++) {
+				 if(this.chosenKey === this.formattedName[1][j]){
+				 	this.lastSpaces[j] = this.chosenKey;
+				 }
+			}
+			document.getElementById('first').innerHTML = this.firstSpaces.join(' ');
+            document.getElementById('last').innerHTML = this.lastSpaces.join(' ');
+			console.log(this.firstSpaces, this.lastSpaces);
 		}
 	}
 }
 
 game.startGame();
-game.chooseName();
+
 
 
 
