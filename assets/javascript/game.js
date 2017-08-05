3b57024dfc81a08ccd0ac7801e535a0ba6cdb8fb
Momojunzi <mark.holthaus@gmail.com>
2017-08-05 01:25:11 -0700

    final

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -20,17 +20,19 @@ var game = {
 		var gameplay = document.getElementById("gameplay-div");
 		var usedDiv = document.getElementById("used-div");
 		var dashesDiv = document.getElementById("dashes-div");
-		console.log(this.gameStarted);
+		var imgDiv = document.getElementById("game-img-div");
+		
 		document.onkeyup = function(event) {
 			if(game.gameStarted === false) {
 				startDiv.style.display = "none";
 				gameplay.style.display = "block";
 				usedDiv.style.display = "block";
 				dashesDiv.style.display = "block";
+				imgDiv.style.display = "block";
 				game.gameStarted = true;
 				game.chooseName();
 				game.keyChooser();
-				console.log(game.gameStarted);
+				
 			}
 		}
 	},
@@ -52,7 +54,7 @@ var game = {
             document.getElementById('first').innerHTML = this.firstSpaces.join(' ');
             document.getElementById('last').innerHTML = this.lastSpaces.join(' ');
             document.getElementById('guess-span').innerHTML = this.guesses;
-            console.log(this.formattedName[0], this.formattedName[1], this.firstSpaces, this.lastSpaces, this.guesses);
+            
 		}
 	},
 	keyChooser: function() {
@@ -91,7 +93,7 @@ var game = {
 			document.getElementById('first').innerHTML = this.firstSpaces.join(' ');
             document.getElementById('last').innerHTML = this.lastSpaces.join(' ');
 			
-		}console.log(this.firstSpaces, this.lastSpaces, this.guesses, this.gameOver);
+		}
 	},
 	winOrLose: function() {
 		if(this.guesses < 1) {
@@ -104,10 +106,13 @@ var game = {
 		}
 		if(this.firstSpaces.indexOf("_") === -1 && this.lastSpaces.indexOf("_") === -1) {
 			//this.gameOver = true;
+			var winName = "./assets/images/" + this.firstSpaces.join('') + ".jpg";
+			console.log(winName);
 			document.getElementById("g-o-t").style.display = "none";
 			document.getElementById("guess-text").style.display = "none";
-			document.getElementById("gameplay-text").innerHTML = "<h1>You survived Joffrey!</h1><h2>For Now...</h2><h4>Press your luck, try again.</h4>";
-			document.getElementById("game-img-div").style.display = "block";
+			document.getElementById("gameplay-text").innerHTML = "<h1>You survived Joffrey!</h1><h3>For Now...</h3><h4>Press your luck, try again.</h4>";
+			document.getElementById("img-wrapper").innerHTML = '<img src=' + winName + ' class="img-responsive game-image">'
+			console.log(document.getElementById("game-img-div").getAttribute("src"));
 			this.reset();
 		}
 	},
